1f7b0cea3ab2403a53ea59f858e20dbcdaad89e8
Aleksandr Sergeev <alexx.sergeev@gmail.com>
2020-12-13 20:28:13 +0500

    edit tests.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -68,16 +68,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':'django.contrib.auth.password_validation'
+               '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 
@@ -121,5 +125,6 @@ PASSWORDLESS_AUTH = {
     'PASSWORDLESS_AUTH_TYPES': ['EMAIL'],
     'PASSWORDLESS_EMAIL_NOREPLY_ADDRESS': 'noreply@example.com',
     'PASSWORDLESS_AUTH_TOKEN_CREATOR': 'api.utils.get_token_for_user',
-    'PASSWORDLESS_AUTH_TOKEN_SERIALIZER': 'api.serializers.JWTTokenResponseSerializer',
+    'PASSWORDLESS_AUTH_TOKEN_SERIALIZER': 'api.serializers'
+                                          '.JWTTokenResponseSerializer',
 }
